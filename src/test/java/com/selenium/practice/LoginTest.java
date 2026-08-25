83406eabe1bd50728f24584a9d18231b613e6ccb
yuvarajgedipudi <yuvarajgedipudi@gmail.com>
2026-08-26 01:40:25 +0530

    added third method in logintest java class

--- src/test/java/com/selenium/practice/LoginTest.java
+++ src/test/java/com/selenium/practice/LoginTest.java
@@ -4,16 +4,22 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    @Test
+    @Test(priority = 1)
     public void loginTestone() {
 
         System.out.println("My first TestNG test  111");
     }
 
-    @Test
+    @Test(priority = 2)
     public void loginTesttwo() {
 
         System.out.println("My first TestNG test 222");
+    }
+
+    @Test(priority = 3)
+    public void loginTestthree() {
+
+        System.out.println("My first TestNG test 333");
     }
 
 
